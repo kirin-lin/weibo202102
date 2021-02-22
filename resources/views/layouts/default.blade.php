284356ad2d61bc40bba43c37686da004f8bd01c9
kirin-lin <chilin.lin@gmail.com>
2021-02-22 13:55:36 +0000

    解決browser 靜態頁面暫存問題

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,15 +2,15 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel Course</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
   </head>
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container">
         <a class="navbar-brand" href="/">Weibo App</a>
         <ul clas="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">HELP</a><li>
-          <li class="nav-item"><a class="nav-link" href="/about">ABOUT</a><li>
+          <li class="nav-item"><a class="nav-link" href="/help">HELP</a></li>
+          <li class="nav-item"><a class="nav-link" href="/about">ABOUT</a></li>
         </ul>
       </div>
     </nav>
