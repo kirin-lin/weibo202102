6d0525b76df32a9db6a41346da7b11e48be629d2
kirin-lin <chilin.lin@gmail.com>
2021-02-22 22:37:37 +0800

    分割 header 及 footer 子視圖

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,5 +3,14 @@
 @section('content')
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
+    <p class='lead'>
+      Here is the main page
+    </p>
+    <p>
+      Everything is starting here.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+    </p>
   </div>
 @stop
