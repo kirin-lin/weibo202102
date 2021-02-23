a764561570e280033e17babe4c8228852a66549e
kirin-lin <chilin.lin@gmail.com>
2021-02-23 09:36:36 +0800

    用戶註冊頁面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -10,7 +10,7 @@
       Everything is starting here.
     </p>
     <p>
-      <a class="btn btn-lg btn-success" href="#" role="button">Register Now</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Register Now</a>
     </p>
   </div>
 @stop
