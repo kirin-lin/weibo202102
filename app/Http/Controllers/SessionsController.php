85715028b7d36d16ab3f8adac8a633f8e53d011f
kirin-lin <chilin.lin@gmail.com>
2021-03-08 06:22:40 +0000

    User Logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -28,4 +28,11 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', "Logout Successful");
+        return redirect('login');
+    }
 }
