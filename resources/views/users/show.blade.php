0cfe9bfe7d3709719277e34162211d16b14dba51
kirin-lin <chilin.lin@gmail.com>
2021-03-19 17:54:19 +0800

    社交統計信息

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -7,6 +7,9 @@
     <section class="user_info">
       @include('shared._user_info', ['user' => $user])
     </section>
+    <section class="stat mt-2">
+      @include('shared._stats', ['user' => $user])
+    </section>
 
     <section class="status">
       @if ($statuses->count() > 0)
